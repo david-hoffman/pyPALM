b9aedddf5044997a91897d5c3870049b912661ba
David Hoffman <dave.p.hoffman@gmail.com>
2018-05-08 12:48:39 -0400

    Fix `find_matches` and `agg_groups`
    find_matches now looks "backwards" meaning that for each point in the
    newest frame corresponding points in the cache are found. Before this
    was done in reverse and it meant that there were potentail duplicates.
    
    agg_groups now calculates the variance as the weighted variance.

--- grouping.py
+++ grouping.py
@@ -35,17 +35,22 @@ def find_matches(frames, radius):
     frame0, frame1 = frames
     t0 = cKDTree(frame0)
     t1 = cKDTree(frame1)
-    pair_matches = t0.query_ball_tree(t1, radius)
+    # backwards looking, find points in frame0 that match points in frame1
+    pair_matches = t1.query_ball_tree(t0, radius)
     # return only the closest match.
     
     def closest_match(m, i):
+        """i is index in frame1, m is index in frame0
+        make sure to return frame0 index first
+        """
         if len(m) == 1:
-            return [i, m[0]]
+            return [m[0], i]
         elif len(m) == 0:
             # should never get here, see list comprehension below
             raise RuntimeError("Something went wrong in `closest_match`")
-        distances = ((t0.data[i] - t1.data[m])**2).sum(1)
-        return [i, m[distances.argmin()]]
+        distances = ((t1.data[i] - t0.data[m])**2).sum(1)
+        # return [frame0_idx, frame1_idx]
+        return [m[distances.argmin()], i]
     
     # return a list with only the closest peak.
     pair_matches = [closest_match(m, i) for i, m in enumerate(pair_matches) if len(m)]
@@ -60,7 +65,8 @@ def group(df, radius, gap, frame_reset=np.inf):
     df : pandas DataFrame
     radius : float
     gap : int
-    frame_reset : int"""
+    frame_reset : int
+    """
     new_df_list = []
     # should add a progress bar here
     frame_min = df.frame.min()
@@ -83,7 +89,6 @@ def group(df, radius, gap, frame_reset=np.inf):
                 # if there is a new peak that matches to two or more different cached peaks then the newer of the
                 # cached peaks claims it. If the cached peaks have the same age then its a toss up.
                 cache_idx, peaks_idx = np.array([[df_cache.index[i], peaks.index[m]] for i, m in matches]).T
-                print(cache_idx, peaks_idx)
             except ValueError as error:
                 # should log the error or raise as a warning.
                 logger.warning(error)
@@ -97,72 +102,66 @@ def group(df, radius, gap, frame_reset=np.inf):
         # update df_cache and lifetimes
         # updating the cache takes a significant amount of time.
         df_cache = pd.concat((df_cache, peaks))
+        # the commented line below is more accurate but about 4-5X slower
+        # df_cache = agg_groups(df_cache).reset_index()
         df_cache = df_cache.drop_duplicates("group_id", "last")
         df_cache = df_cache[(frame - df_cache.frame) < gap]
         new_df_list.append(peaks)
     return pd.concat(new_df_list)
 
 
-# def agg_groups(df_grouped):
-#     def f(x):
-#         if len(x) < 2:
-#             x = x.drop("group_id", 1)
-#             x["groupsize"] = 1
-#             return x
-        
-#         coords = ["x", "y", "z"]
-#         x0s = [c + '0' for c in coords]
-#         sigmas = ["sigma_" + c for c in coords]
-        
-#         d = pd.DataFrame(np.nan, columns=x0s + sigmas, index=x.index[:1])
-#         inv_sigma = (1 / x[sigmas].values**2).sum(0)
-#         d[sigmas] = 1 / np.sqrt(inv_sigma)
-        
-#         d[x0s] = (x[x0s].values / x[sigmas].values**2).sum(0) / inv_sigma
-            
-#         d["nphotons"] = x["nphotons"].sum()
-#         d["amp"] = x["amp"].sum()
-#         d["offset"] = x["offset"].mean()
-#         d["groupsize"] = len(x)
-#         d["frame"] = x["frame"].iloc[0]
-
-#         return d
-
-#     a = df_grouped.groupby("group_id").apply(f)
-#     a.index = a.index.droplevel(1)
-#     return a
-
-
 def agg_groups(df_grouped):
-    # define coordinates
+    """Aggregate groups
+    
+    https://en.wikipedia.org/wiki/Weighted_arithmetic_mean#Weighted_sample_variance
+    """
     coords = ["x", "y", "z"]
     # save the labels for weighted coords and weights
     w_coords = []
+    w_coords2 = []
     weights = []
     # loop through coords generating weights and weighted coords
     for c in coords:
+        # calculate weights
         s = "sigma_" + c
         df_grouped[s + "_inv"] = 1 / df_grouped[s] ** 2
         weights.append(s + "_inv")
         x = c + "0"
+        # weighted position
         df_grouped[x + "_w"] = df_grouped[x].mul(df_grouped[s + "_inv"], "index")
         w_coords.append(x + "_w")
+        # weighted position^2
+        df_grouped[x + "_w2"] = (df_grouped[x]**2).mul(df_grouped[s + "_inv"], "index")
+        w_coords2.append(x + "_w2")
     # groupby group_id and sum
     temp_gb = df_grouped.groupby("group_id")
+    # calculate sum weights
+    sum_w = temp_gb[weights].sum().values
     # finish weighted mean
-    new_coords = temp_gb[w_coords].sum() / temp_gb[weights].sum().values
+    new_coords = temp_gb[w_coords].sum() / sum_w
+    # doing this here to preserve order
+    new_coords2_values = (new_coords**2)[w_coords].values
     new_coords.columns = [c.replace("_w", "") for c in new_coords.columns]
     # calc new sigma
-    new_sigmas = np.sqrt(1 / temp_gb[weights].sum())
-    new_sigmas.columns = [c.replace("_inv", "") for c in new_sigmas.columns]
+    # basically the weighted std dev of the points added in quadrature to the 
+    # std dev of the std dev of all points
+    # $$
+    # \sqrt{\frac{\sum w_i (x_i - \mu^*)^2}{\sum w_i} + \frac{1}{\sum w_i}} \\
+    # \sqrt{\frac{\sum w_i x_i^2}{\sum w_i} - \mu^{*2} + \frac{1}{\sum w_i}} \\
+    # \sqrt{\frac{\sum w_i x_i^2 + 1}{\sum w_i} - \mu^{*2}} \\
+    # w_i = \frac{1}{\sigma_i^2}
+    # $$
+    new_sigmas = np.sqrt((temp_gb[w_coords2].sum() + 1) / sum_w -
+                         new_coords2_values)
+    new_sigmas.columns = ["sigma_" + c[0] for c in new_sigmas.columns]
     # calc new group params
     new_amp = temp_gb[["amp", "nphotons", "chi2"]].sum()
-    new_frame = temp_gb[["frame"]].first()
+    new_frame = temp_gb[["frame"]].last()
     groupsize = temp_gb.x0.count()
     groupsize.name = "groupsize"
     new_offset = temp_gb[["offset"]].mean()
     # drop added columns from original data frame
-    df_grouped.drop(columns=w_coords + weights, inplace=True)
+    df_grouped.drop(columns=w_coords + weights + w_coords2, inplace=True)
     # return new data frame
     return pd.concat([new_coords, new_sigmas, new_amp, new_frame, groupsize, new_offset], axis=1)
 
